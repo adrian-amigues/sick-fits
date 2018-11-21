c2a90469764d5b5ce6edec55acdfd542b2132ba4
adrian-amigues <adrian.amigues@gmail.com>
2018-11-21 17:26:47 +0100

    Add graphql server items query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if user logged in
 
-module.exports = Mutations;
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    )
+
+    return item
+  },
+}
+
+module.exports = Mutations
