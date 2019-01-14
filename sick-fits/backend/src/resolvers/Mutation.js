25f420b4cae82856b6ba1cef3ec0ec8a7cd0c97a
adrian-amigues <adrian.amigues@gmail.com>
2019-01-14 10:27:31 +0100

    Add updateItem and deleteItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -13,6 +13,29 @@ const Mutations = {
 
     return item
   },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args }
+    // remove the ID from the updates
+    delete updates.id
+    // run the updates methos
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id },
+      },
+      info
+    )
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // 1 find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`)
+    // 2 Check if they own that item or have the permission
+    // TODO
+    // 3 Delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
+  },
 }
 
 module.exports = Mutations
--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -41,6 +41,26 @@ export default class CreateItem extends Component {
     this.setState({ [name]: val })
   }
 
+  uploadFile = async e => {
+    console.log('Uploading File')
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dhsilsmb0/image/upload',
+      { method: 'POST', body: data }
+    )
+    const file = await res.json()
+    console.log('file: ', file)
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -61,17 +81,20 @@ export default class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
-              <label htmlFor="title">
-                Title
+              <label htmlFor="file">
+                Image
                 <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  placeholder="Title"
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
                   required
-                  value={this.state.title}
-                  onChange={this.handleChange}
+                  // value={this.state.image}
+                  onChange={this.uploadFile}
                 />
+                {this.state.image && (
+                  <img src={this.state.image} alt="Upload preview" />
+                )}
               </label>
 
               <label htmlFor="price">
